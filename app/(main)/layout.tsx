301458aafb515b7c1d2054ca6a6fef126083840f
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-11-21 20:59:53 +0100

    layout sidebar and main content structure

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,15 +1,22 @@
+import Sidebar from "@/components/sidebar"
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
-        {/* TODO: Navbar */}
-        <p>Navbar</p>
-        {children}
-      </body>
-    </html>
+    <section>
+      <div className="flex">
+        {/* Sidebar */}
+        <div className="w-1/5">
+          <Sidebar/>
+        </div>
+        {/* Main content */}
+        <div className="flex-1">
+            {children}
+        </div>
+      </div>
+    </section>
   )
 }
