6ccb997bf6fb3e22886be7938f81a9049f69412d
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-11-22 12:23:55 +0100

    Add sign in form

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import SignInForm from "@/components/SignInForm"
+import Image from "next/image"
+import React from "react"
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <section className="h-screen w-full flex justify-between px-60 items-center">
+      <div>
+        <Image
+          width={400}
+          height={400}
+          src={"/brandinglogo.png"}
+          alt="brandinglogo"
+        />
+      </div>
+      <div>
+        <Image src="/ek_logo.png" width={400} height={100} alt="logo" />
+        <SignInForm />
+      </div>
+    </section>
   )
 }
 
--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+'use client'
 
-const SignInForm = () => {
+import {
+  Button,
+  Container,
+  Paper,
+  PasswordInput,
+  TextInput,
+} from "@mantine/core"
+
+export default function SignInForm() {
+
+
+  // Sign in handler
+  const handleSignIn = async (e: any) => {
+    e.preventDefault()
+
+    console.log("Signed in")
+  }
+
   return (
-    <div>SignInForm</div>
+    <Container size={420}>
+      <form onSubmit={handleSignIn}>
+        <Paper p={22} radius="md">
+          <TextInput
+            label="Email"
+            placeholder="you@mantine.dev"
+            required
+            radius="md"
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            required
+            mt="md"
+            radius="md"
+          />
+          <Button type="submit" color="#0339A7" fullWidth mt="xl" radius="md">
+            Sign in
+          </Button>
+        </Paper>
+      </form>
+    </Container>
   )
 }
-
-export default SignInForm
