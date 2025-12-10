7bb3879644f42ded5727a23a54cc4f9ae64e64df
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-12-10 19:41:40 +0100

    responsive signin page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -3,14 +3,25 @@ import Image from "next/image"
 
 const SignIn = () => {
   return (
-    <section className="h-screen w-full flex justify-between px-60 items-center">
+    <section className="min-h-screen w-full flex flex-col gap-8 md:flex-row sm:justify-between p-6 xl:px-60 items-center">
       <div>
-        <Image
-          width={400}
-          height={400}
-          src={"/brandinglogo.png"}
-          alt="brandinglogo"
-        />
+        {/* Mobile image */}
+        <div className="block sm:hidden">
+          <Image
+            width={250}
+            height={250}
+            src={"/brandinglogo.png"}
+            alt="brandinglogo"
+          />
+        </div>
+        <div className="hidden sm:block">
+          <Image
+            width={400}
+            height={400}
+            src={"/brandinglogo.png"}
+            alt="brandinglogo"
+          />
+        </div>
       </div>
       <div>
         <Image className="mb-10" src="/logo.png" width={400} height={100} alt="logo" />
